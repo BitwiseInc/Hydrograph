5e4bc79088a94405b56dcad8cd26fb8bbf51bf5b
shivarajn <Shivaraj.Nangare@bitwiseglobal.com>
2017-03-23 09:44:24 +0530

    Updated to add methods : toInteger toFloat toBigdecimal

--- hydrograph.engine/hydrograph.engine.transformation/src/test/java/hydrograph/engine/transformation/standardfunctions/NumericFunctionsTest.java
+++ hydrograph.engine/hydrograph.engine.transformation/src/test/java/hydrograph/engine/transformation/standardfunctions/NumericFunctionsTest.java
@@ -14,63 +14,110 @@ package hydrograph.engine.transformation.standardfunctions;
 
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.text.ParseException;
+
 /**
  * The Class NumericFunctionsTest.
  *
  * @author Bitwise
- *
  */
+
 public class NumericFunctionsTest {
 
-	@Test
-	public void testRound() {
-		Float result1 = NumericFunctions.round(1.234f, 2);
-		Double result2 = NumericFunctions.round(-1.234, 2);
-		Double result3 = NumericFunctions.round(1.235, 2);
-		Assert.assertEquals(result1,new Float(1.23f));
-		Assert.assertEquals(result2,new Double(-1.23));
-		Assert.assertEquals(result3,new Double(1.24));
-	}
+    @Test
+    public void testRound() {
+        Float result1 = NumericFunctions.round(1.234f, 2);
+        Double result2 = NumericFunctions.round(-1.234, 2);
+        Double result3 = NumericFunctions.round(1.235, 2);
+        Assert.assertEquals(result1, new Float(1.23f));
+        Assert.assertEquals(result2, new Double(-1.23));
+        Assert.assertEquals(result3, new Double(1.24));
+    }
 
-	@Test
-	public void testRoundUp() {
-		Float result1 = NumericFunctions.roundUp(231.3341f, 3);
-		Double result2 = NumericFunctions.roundUp(1.235, 2);
-		Assert.assertEquals(result1,new Float(231.335));
-		Assert.assertEquals(result2,new Double(1.24));
-	}
+    @Test
+    public void testRoundUp() {
+        Float result1 = NumericFunctions.roundUp(231.3341f, 3);
+        Double result2 = NumericFunctions.roundUp(1.235, 2);
+        Assert.assertEquals(result1, new Float(231.335));
+        Assert.assertEquals(result2, new Double(1.24));
+    }
 
-	@Test
-	public void testRoundDown() {
-		Double result1 = NumericFunctions.roundDown(231.3341, 3);
-		Double result2 = NumericFunctions.roundDown(231.3346, 3);
-		Assert.assertEquals(result1,new Double(231.334));
-		Assert.assertEquals(result2,new Double(231.334));
-	}
+    @Test
+    public void testRoundDown() {
+        Double result1 = NumericFunctions.roundDown(231.3341, 3);
+        Double result2 = NumericFunctions.roundDown(231.3346, 3);
+        Assert.assertEquals(result1, new Double(231.334));
+        Assert.assertEquals(result2, new Double(231.334));
+    }
 
-	@Test
-	public void testTruncate() {
-		Double result1 = NumericFunctions.truncate(1.219,2);
-		Double result2 = NumericFunctions.truncate(123.456,1);
-		Assert.assertEquals(result1,new Double(1.21));
-		Assert.assertEquals(result2,new Double(123.4));
-	}
+    @Test
+    public void testTruncate() {
+        Double result1 = NumericFunctions.truncate(1.219, 2);
+        Double result2 = NumericFunctions.truncate(123.456, 1);
+        Assert.assertEquals(result1, new Double(1.21));
+        Assert.assertEquals(result2, new Double(123.4));
+    }
 
-	@Test
-	public void testCeil() {
-		Double result = NumericFunctions.ceil(1.235);
-		Assert.assertEquals(result,new Double(2.0));
-	}
+    @Test
+    public void testCeil() {
+        Double result = NumericFunctions.ceil(1.235);
+        Assert.assertEquals(result, new Double(2.0));
+    }
 
-	@Test
-	public void testFloor() {
-		Double result = NumericFunctions.floor(1.235);
-		Assert.assertEquals(result,new Double(1.0));
-	}
+    @Test
+    public void testFloor() {
+        Double result = NumericFunctions.floor(1.235);
+        Assert.assertEquals(result, new Double(1.0));
+    }
 
-	@Test
-	public void testLength() {
-		Integer length = NumericFunctions.length(1000);
-		Assert.assertEquals(length,new Integer(4));
-	}
+    @Test
+    public void testLength() {
+        Integer length = NumericFunctions.length(1000);
+        Assert.assertEquals(length, new Integer(4));
+    }
+
+    @Test
+    public void itShouldValidateToInteger() {
+        Integer integer = new Integer(23);
+
+        Assert.assertEquals(integer, NumericFunctions.toInteger("23"));
+    }
+
+    @Test
+    public void itShouldValidateToFloat() {
+        Float float1 = new Float(2421.123);
+        Integer integer1 = new Integer(12);
+        Long long1 = new Long(324L);
+        Double double1 = new Double(12.2345);
+        BigDecimal bigDecimal1 = new BigDecimal(1231.2).setScale(2, BigDecimal.ROUND_DOWN);
+
+
+        Assert.assertEquals(float1, NumericFunctions.toFloat("2421.123"));
+        Assert.assertEquals(new Float(integer1), NumericFunctions.toFloat(integer1));
+        Assert.assertEquals(new Float(long1), NumericFunctions.toFloat(long1));
+        Assert.assertEquals(new Float(double1), NumericFunctions.toFloat(double1));
+        Assert.assertEquals(new Float(bigDecimal1.floatValue()), NumericFunctions.toFloat(bigDecimal1));
+    }
+
+    @Test
+    public void istShouldValidateToBigDecimal() {
+
+        Integer integer1 = new Integer(12);
+        Long long1 = new Long(324L);
+        Float float1 = new Float(2421.123);
+        Double double1 = new Double(12.2345);
+        char[] charArray = {'1', '2', '.', '5'};
+        try {
+            Assert.assertEquals(new BigDecimal(12.39).setScale(2, BigDecimal.ROUND_DOWN), NumericFunctions.toBigdecimal("12.39", 2));
+            Assert.assertEquals(new BigDecimal(integer1), NumericFunctions.toBigdecimal(integer1));
+            Assert.assertEquals(new BigDecimal(long1), NumericFunctions.toBigdecimal(long1));
+            Assert.assertEquals(new BigDecimal(float1), NumericFunctions.toBigdecimal(float1));
+            Assert.assertEquals(new BigDecimal(double1).setScale(4, BigDecimal.ROUND_DOWN), NumericFunctions.toBigdecimal(double1));
+            Assert.assertEquals(new BigDecimal(12.5).setScale(1, BigDecimal.ROUND_DOWN), NumericFunctions.toBigdecimal(charArray, 1));
+        } catch (ParseException parseException) {
+            parseException.printStackTrace();
+        }
+    }
 }
