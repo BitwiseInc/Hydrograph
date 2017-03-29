c5a8cb784c559bc0aa8381db3b2f33e14faf8609
Sonia Raheja <sonia.raheja@bitwiseglobal.com>
2017-03-29 16:57:13 +0530

    Remove unused imports.

--- hydrograph.ui/hydrograph.ui.propertywindow/src/main/java/hydrograph/ui/propertywindow/utils/Utils.java
+++ hydrograph.ui/hydrograph.ui.propertywindow/src/main/java/hydrograph/ui/propertywindow/utils/Utils.java
@@ -29,7 +29,6 @@ import java.util.Properties;
 import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.lang.StringUtils;
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
